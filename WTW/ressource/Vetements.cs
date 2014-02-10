6beda49f52024efd1b8f1fc7b4b96413f2184f7a
GravityReset <152565@supinfo.com>
2014-02-10 15:54:13 +0100

    first commit

---
+++ WTW/ressource/Vetements.cs
@@ -0,0 +1,135 @@
+﻿using System.ComponentModel;
+
+namespace WTW.ressource
+{
+    public class Vetements : INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private bool parapluie;
+        private int haut;
+        private int bas;
+        private bool lunetteDeSoleil;
+
+        public bool IsTopVisible
+        {
+            get { return LunetteDeSoleil || Parapluie; }
+        }
+        public bool LunetteDeSoleil
+        {
+            get { return lunetteDeSoleil; }
+            set { lunetteDeSoleil = value; OnPropertyChanged("LunetteDeSoleil"); OnPropertyChanged("IsTopVisible"); }
+        }
+
+        //TODO onproperty change Bas et Haut peut etre inutile
+        public int Bas
+        {
+            get { return bas; }
+            set { bas = value; OnPropertyChanged("TexteBas"); OnPropertyChanged("VetBas"); }
+        }
+        public int Haut
+        {
+            get { return haut; }
+            set { haut = value; OnPropertyChanged("TexteHaut"); OnPropertyChanged("VetHaut"); }
+        }
+        public string VetHaut { get { return @"im\shirt" + Haut.ToString()+".png"; } }
+        public string VetBas { get { return @"im\short" + Bas.ToString()+".png"; } }
+        public bool Parapluie
+        {
+            get { return parapluie; }
+            set { parapluie = value; OnPropertyChanged("Parapluie"); OnPropertyChanged("IsTopVisible"); }
+        }
+        public string TexteHaut { get { return getTextHaut(); } }
+        public string TexteBas { get { return getTextBas(); } }
+
+        private string getTextHaut()
+        {
+            switch (Haut)
+            {
+                case 1:
+                    return "Un T-shirt ou un débardeur bien léger, histoire de pas avoir trop chaud.";
+                case 2:
+                    return "Un T-shirt c'est possible et pour les frileux un à manche longue.";
+                case 3:
+                    return "Il commence à faire froid il faut se couvrir un petit pull est de mise.";
+                case 4:
+                    return "Il fait froid il faut se couvrir et mettre un bon gros pull.";
+                default:
+                    return string.Empty;
+            }
+        }
+
+        private string getTextBas()
+        {
+            switch (Bas)
+            {
+                case 1:
+                    return "Un short ou une jupe, idéale quand il fait chaud.";
+                case 2:
+                    return "Un pantacourt ca peut-etre une bonne idée ni trop chaud ni trop froid.";
+                case 3:
+                    return "Mets un pantalon sauf si tu es courageux!";
+                default:
+                    return string.Empty;
+            }
+        }
+
+
+        public Vetements(meteo met)
+        {
+            LunetteDeSoleil = false;
+
+            Parapluie = false;
+            Haut = 0;
+            Update(met);
+        }
+
+
+        public void Update(meteo met)
+        {
+            Parapluie = false;
+            LunetteDeSoleil = false;
+            if (met.IsRaining > 0.2M)
+                Parapluie = true;
+            if (met.Clouds < 15 && met.TempMax > 14)
+                LunetteDeSoleil = true;
+
+            //haut+bas
+            if (met.Temp < 8 || met.TempMin < 4)
+            {
+                Haut = 4;
+                Bas = 3;
+            }
+            else if ((met.IsRaining > 2 && met.TempMin < 23) || met.TempMax < 18)
+            {
+                Haut = 3;
+                Bas = 3;
+            }
+            else if (met.Temp < 25)
+                Haut = 2;
+            else
+                Haut = 1;
+
+            if (met.IsRaining > 2 && met.Temp < 25)
+                Bas = 3;
+            else if (met.Temp > 28)
+                Bas = 1;
+            else if (met.Temp > 23)
+                Bas = 2;
+            else
+                Bas = 3;
+            //fin haut+bas
+        }
+
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+}
---
+++ WTW/ressource/Weather.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Xml.Linq;
+using Windows.Data.Xml.Dom;
+using Windows.UI.Popups;
+
+namespace WTW.ressource
+{
+
+    public class Weather : INotifyPropertyChanged
+    {
+        private const string Err404 = "{\"message\":\"\",\"cod\":\"404\"}\n";
+        public event PropertyChangedEventHandler PropertyChanged;
+        private string location;
+        private string city;
+        private string pays;
+
+
+        private ObservableCollection<meteo> meteoP;
+        public ObservableCollection<meteo> Meteo { get { return meteoP; } private set { meteoP = value; } }
+        public string City { get { return city; } set { city = value; OnPropertyChanged("City"); OnPropertyChanged("PaysFull"); } }
+        public string PaysFull
+        {
+            get { return City + " " + Pays; }
+        }
+        public string Pays { get { return pays; } set { pays = value; OnPropertyChanged("Pays"); OnPropertyChanged("PaysFull"); } }
+
+        public Weather()
+        {
+            this.location = Param.Location;
+            City = string.Empty;
+            Meteo = new ObservableCollection<meteo>();
+            for (int i = 0; i < 7; i++)
+                Meteo.Add(new meteo(i));
+
+        }
+
+        /// <summary>
+        /// recupere les information météo
+        /// </summary>
+        /// <returns>renvoie string.Empty si tout a fonctionner sinon renvoie lemessage d'érreur</returns>
+        public async void GetCurrentConditions(bool refresher = false)
+        {
+            if (refresher)
+                location = Param.Location;
+            try
+            {
+                var xml = await new System.Net.Http.HttpClient().GetStringAsync(
+                    string.Format("http://api.openweathermap.org/data/2.5/forecast/daily?q={0}&mode=xml&units=metric&cnt={1}&lang={2}",
+                    location, 7, "fr"));
+
+                xmlMeteo(xml);
+            }
+            catch (Exception ex)
+            {
+                MessageDialog msgDialog = new MessageDialog(ex.Message, "Erreur chargement meteo");
+                msgDialog.ShowAsync();
+            }
+        }
+
+        public async void GetCurrentConditions(double longitude, double lattitude)
+        {
+            try
+            {
+                string xml = await new System.Net.Http.HttpClient().GetStringAsync(
+                    string.Format("http://api.openweathermap.org/data/2.5/forecast/daily?lat={0}&lon={1}&mode=xml&units=metric&cnt={2}&lang={3}",
+                     lattitude, longitude, 7, "fr"));
+
+                //read from xmlDocument for your values.
+                xmlMeteo(xml);
+                location = City;
+                Param.Location = location;
+            }
+            catch (Exception ex)
+            {
+                MessageDialog msgDialog = new MessageDialog(ex.Message, "Erreur chargement meteo");
+                msgDialog.ShowAsync();
+            }
+        }
+
+        private void xmlMeteo(string xml)
+        {
+            if (xml != Err404)
+            {
+                var xmlDocument = XDocument.Parse(xml);
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(xmlDocument.ToString());
+                City = doc.GetElementsByTagName("name").First().InnerText;
+                Pays = doc.GetElementsByTagName("country").First().InnerText;
+                foreach (meteo met in Meteo)
+                    met.getMeteo(doc);
+            }
+            else
+                throw new NotSupportedException("Ville introuvable!! Essayez de nouveau :)");
+        }
+
+        protected void OnPropertyChanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+    }
+
+
+}
